54213b10f578ac8fde0513a411e11a09619dc7c3
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-02-12 17:57:54 +0330

    Added functions for slides

---
+++ script.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currenSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
