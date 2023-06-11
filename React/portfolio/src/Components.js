cd10507cd8eb349be0c839a57de5733fac366e06
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-06-11 20:18:42 +0330

    First chages

---
+++ React/portfolio/src/Components.js
@@ -0,0 +1,31 @@
+function Navbar () {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+function Container () {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+function Sections () {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+function Footer () {
+    return(
+        <div>
+            
+        </div>
+    );
+}
