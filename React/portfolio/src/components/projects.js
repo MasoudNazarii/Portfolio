cee9e8661b6bee9c272f6c512babdb08109b978f
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-02 21:22:04 +0330

    delete unnecessary import

--- React/portfolio/src/components/projects.js
+++ React/portfolio/src/components/projects.js
@@ -1,36 +1,10 @@
-import Images from '../images/index.js';
+import Slides from "./slides";
 
 export default function Projects() {
     return(
         <>
             <h3 className="pro-h">Projects</h3>
-            <div className="slideshow-container">
-                <div className="slides fade">
-                    <img
-                    className="slide-img"
-                    src={Images.WeatherApp}
-                    alt="Weather-App"
-                    />
-                    <div className="overlay">
-                        <div className="info">
-                            Just a simple weather app website if you want go check it. :)
-                        </div>
-                    </div>
-                    <p className="caption">Wheather App</p>
-                </div>
-                <div className="slides fade">
-                    <img
-                    className="slide-img"
-                    src={Images.Calculator}
-                    alt="Calculator"
-                    />
-                    <div className="overlay">
-                        <div className="info">another simple project a calculator.</div>
-                    </div>
-                    <p className="caption">Calculator</p>
-                </div>
-                
-            </div>
+            <Slides />
         </>
     );
 }
