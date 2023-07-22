6ee933273b7ba3edec3cb878deef209b7622dee6
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-22 22:16:26 +0330

    remove deleted pictures path

--- src/images/index.js
+++ src/images/index.js
@@ -4,9 +4,6 @@ const Images = {
     Icon: require('./Icon.ico'),
     LogoLight: require('./Logo-light.png'),
     Logo: require('./Logo.png'),
-    Maciek: require('./maciek-sulkowski-43gFXiPsMbY-unsplash.jpg'),
-    Neha: require('./neha-maheen-mahfin-uGcN5XEZzoc-unsplash.jpg'),
-    Pramod: require('./A.jpg'),
     Sina: require('./sina-rezakhani-0FpRnQl0IfE-unsplash.jpg'),
     WeatherApp: require('./WeatherApp.png')
 }
