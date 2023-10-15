7a31295fea471fea738ad4e74e55e8d7950bf8d7
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-10-15 21:42:07 +0330

    update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,21 @@
 import Images from '../images/index.js';
 
 export default function Navbar() {
+    window.onscroll = function() {scrollFunction()};
+ 
+    function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.background = "#1e1e1e2a";
+        document.getElementById("navbar").style.borderBottom = "1px solid #7a7a7a4b";
+    }
+    else {
+        document.getElementById("navbar").style.background = "none";
+        document.getElementById("navbar").style.borderBottom = "none";
+    }}
+
     return(
         <>
-            <nav className="navbar">
+            <nav id='navbar' className="navbar">
                 <div className="left-items">
                     <a href="#Home">Home</a>
                     <a href="#Contact">Contact</a>
@@ -12,7 +24,7 @@ export default function Navbar() {
                 <div className="right-items">
                     <img
                     className="logo"
-                    src={Images.Logo}
+                    src={Images.LogoLight}
                     alt="logo"
                     />
                 </div>
