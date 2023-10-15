448576489f68d3ec4720a9fb1485c24ff00d1764
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-10-15 21:41:45 +0330

    edit about

--- src/components/about.js
+++ src/components/about.js
@@ -11,20 +11,20 @@ export default function About() {
             alt="proftest"
             />
             <div className="about">
-            <h3>About</h3>
-            <div id="changeText" style={{ color: "rgb(219, 143, 0)" }} >
+            <h3>ABOUT</h3>
+            <div id="changeText">
                 <Typewriter 
                     options={{
-                        strings: ['Programer', 'Gamer', 'Student'],
+                        strings: ['Programer', 'Developer', 'Student', 'Front-end Developer'],
                         loop: true,
                         autoStart: true
                     }}
                     
                 />
             </div>
-            <p style={{ color: "rgb(172, 172, 172)" }} >
-                Hello, I'm Masoud Nazari, a beginner front-end developer. I have experience with HTML, CSS, and JavaScript, and I've worked on several projects.<br/>
-                Right now, I'm learning React.js to expand my skills.<br/>
+            <p>
+                I am Masoud, a passionate front-end developer.<br/>I have experience with HTML, CSS, and JavaScript,<br/>and I have worked on several projects.<br/>
+                Right now, I am learning React.js to expand my skills.<br/>
                 Feel free to check out some of my projects at{" "}
                 <a id="link-p" href="#Projects">
                     here
